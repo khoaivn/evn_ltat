10088852c1a227afa408813dfe25f9c0d6055f3a
khoai <trungdunght2003@gmail.com>
2024-09-16 00:48:31 +0700

    First

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";  // Trả về tên file index.html trong thư mục templates
+    }
+}
